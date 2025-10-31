d594c4b5feb5a40e33c6c0484fd125ac0935c23d
이학인 <dlgkrdls304@naver.com>
2025-10-31 15:56:56 +0900

    Replace BrowserRouter with HashRouter

--- src/main.tsx
+++ src/main.tsx
@@ -1,14 +1,13 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App'
-import { BrowserRouter, HashRouter } from 'react-router-dom'
-
-const Router = import.meta.env.VITE_ROUTER_MODE === 'hash' ? HashRouter : BrowserRouter
+import { HashRouter } from 'react-router-dom'
+import App from "./App";
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <Router>
+    <HashRouter>
       <App />
-    </Router>
+    </HashRouter>
   </React.StrictMode>,
 )
