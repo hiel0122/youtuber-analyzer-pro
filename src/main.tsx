d4b579ba7acbd0ec48d2ce9ef9154cb2f7d8fe14
이학인 <dlgkrdls304@naver.com>
2025-10-31 14:10:20 +0900

    Refactor main.tsx to include routing support

--- src/main.tsx
+++ src/main.tsx
@@ -1,5 +1,14 @@
-import { createRoot } from "react-dom/client";
-import App from "./App.tsx";
-import "./index.css";
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import App from './App'
+import { BrowserRouter, HashRouter } from 'react-router-dom'
 
-createRoot(document.getElementById("root")!).render(<App />);
+const Router = import.meta.env.VITE_ROUTER_MODE === 'hash' ? HashRouter : BrowserRouter
+
+ReactDOM.createRoot(document.getElementById('root')!).render(
+  <React.StrictMode>
+    <Router>
+      <App />
+    </Router>
+  </React.StrictMode>,
+)
